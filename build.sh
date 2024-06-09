fc5ed9bcafb8fbd8388a62b15ef5e909c19b9476
Diego Castro <diegof.castro.r@gmail.com>
2024-06-08 20:28:03 -0500

    Ajuste mfiles

--- build.sh
+++ build.sh
@@ -7,17 +7,18 @@ pip install --upgrade pip
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
+echo "Contents of the project directory:"
+ls -l
+ls -ld mediafiles
+chmod 755 mediafiles
+ls -ld mediafiles
+
 # Convert static asset files
 python manage.py collectstatic --noinput
 
 # Apply any outstanding database migrations
 python manage.py migrate
 
-echo "Contents of the project directory:"
-ls -l
-ls -ld mediafiles
-chmod 755 media
-ls -ld mediafiles
 
 # Set the environment variables for the superuser
 # export DJANGO_SUPERUSER_USERNAME=admin
